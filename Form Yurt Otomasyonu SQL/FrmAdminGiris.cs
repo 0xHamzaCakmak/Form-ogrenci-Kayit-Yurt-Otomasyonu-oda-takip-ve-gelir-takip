71414950fe03a17e4b49e52e12f4329af8ad8714
hamza çakmak <hamzaacakmak>
2019-09-29 16:57:45 +0300

    Add project files.

---
+++ Form Yurt Otomasyonu SQL/FrmAdminGiris.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Yurt_Otomasyonu_SQL
+{
+    public partial class FrmAdminGiris : Form
+    {
+        public FrmAdminGiris()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "admin3" && textBox2.Text == "010101*")
+            {
+                FrmAnaForm frm = new FrmAnaForm();
+                frm.Show();
+                this.Hide();
+            }
+        }
+    }
+}
---
+++ Form Yurt Otomasyonu SQL/FrmAnaForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Yurt_Otomasyonu_SQL
+{
+    public partial class FrmAnaForm : Form
+    {
+        public FrmAnaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmAnaForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'yurtKayitDataSet2.Ogrenci' table. You can move, or remove it, as needed.
+            this.ogrenciTableAdapter.Fill(this.yurtKayitDataSet2.Ogrenci);
+            timer1.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongDateString();
+            label2.Text = DateTime.Now.ToLongTimeString();
+
+        }
+
+        private void hesapMakinesiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Calc.exe");
+        }
+
+        private void paintToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("MsPaint.exe");
+        }
+
+        private void radyo1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://www.canli-radyo.biz/";
+        }
+
+        private void radyo2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://www.canli-radyo.biz/2013/07/joy-turk.html";
+        }
+
+        private void radyo3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://www.canli-radyo.biz/2013/07/kral-fm-dinle.html";
+        }
+    }
+}
